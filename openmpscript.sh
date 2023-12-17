849e8e8f1c2c913d30f14c49a205e9ce83fb41a8
FSIS-IBM <154080697+FSIS-IBM@users.noreply.github.com>
2023-12-17 20:23:02 +0000

    Add files via upload

---
+++ openmpscript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N "job_1"
+#PBS -q workq
+#PBS -l select=1:ncpus=24
+#PBS -o Output.out
+#PBS -e error.err
+#PBS -V
+
+
+cd $PBS_O_WORKDIR
+
+ulimit -s unlimited
+export OMP_NUM_THREADS=24
+./dip63.out > output.txt
